c481ae9144b2b2e74a2317d30b3be08d61612c35
Matius Andreatna <matius.andreatna@gmail.com>
2020-07-19 17:39:28 +0700

    Pembuatan Geladak dan Fungsi Zoom out dan Zoom in

--- TR_GRAFKOM_672018072/TR_GRAFKOM_672018072.cpp
+++ TR_GRAFKOM_672018072/TR_GRAFKOM_672018072.cpp
@@ -4,6 +4,7 @@
 
 int w = 1024, h = 1024;
 static float ypoz = 0, zpoz = 0, xpoz = 0, a = 0, b = 0, c = -10;
+static float xScale = 1, yScale = 1, zScale = 1;
 
 float xrot = 0.0f;
 float yrot = 0.0f;
@@ -11,6 +12,75 @@ float xdiff = 0.0f;
 float ydiff = 0.0f;
 bool mouseDown = false;
 int is_depth;
+
+void geladak() {
+    //kanan_belakang
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-45.0f, -5.0f, 12.0f);
+    glVertex3f(-40.0f, 5.0f, 12.0f);
+    glVertex3f(40.0f, 5.0f, 12.0f);
+    glVertex3f(20.0f, -5.0f, 12.0f);
+    glEnd();
+    //kiri_belakang
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-45.0f, -5.0f, -12.0f);
+    glVertex3f(-40.0f, 5.0f, -12.0f);
+    glVertex3f(40.0f, 5.0f, -12.0f);
+    glVertex3f(20.0f, -5.0f, -12.0f);
+    glEnd();
+    //belakang
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(-45.0f, -5.0f, 12.0f);
+    glVertex3f(-40.0f, 5.0f, 12.0f);
+    glVertex3f(-40.0f, 5.0f, -12.0f);
+    glVertex3f(-45.0f, -5.0f, -12.0f);
+    glEnd();
+    //kanan_depan
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(40.0f, 5.0f, 12.0f);
+    glVertex3f(20.0f, -5.0f, 12.0f);
+    glVertex3f(50.0f, -5.0f, 0.0f);
+    glVertex3f(60.0f, 5.0f, 0.0f);
+    glEnd();
+    //kiri_depan
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex3f(40.0f, 5.0f, -12.0f);
+    glVertex3f(20.0f, -5.0f, -12.0f);
+    glVertex3f(50.0f, -5.0f, 0.0f);
+    glVertex3f(60.0f, 5.0f, 0.0f);
+    glEnd();
+    //atas
+    glBegin(GL_QUADS);
+    glColor3f(0.9f, 0.9f, 0.9f);
+    glVertex3f(-40.0f, 5.0f, 12.0f);
+    glVertex3f(40.0f, 5.0f, 12.0f);
+    glVertex3f(40.0f, 5.0f, -12.0f);
+    glVertex3f(-40.0f, 5.0f, -12.0f);
+    glEnd();
+    glBegin(GL_TRIANGLES);
+    glVertex3f(40.0f, 5.0f, 12.0f);
+    glVertex3f(40.0f, 5.0f, -12.0f);
+    glVertex3f(60.0f, 5.0f, 0.0f);
+    glEnd();
+    //bawah
+    glBegin(GL_QUADS);
+    glColor3f(0.9f, 0.9f, 0.9f);
+    glVertex3f(-45.0f, -5.0f, 12.0f);
+    glVertex3f(20.0f, -5.0f, 12.0f);
+    glVertex3f(20.0f, -5.0f, -12.0f);
+    glVertex3f(-45.0f, -5.0f, -12.0f);
+    glEnd();
+    glBegin(GL_TRIANGLES);
+    glVertex3f(20.0f, -5.0f, 12.0f);
+    glVertex3f(20.0f, -5.0f, -12.0f);
+    glVertex3f(50.0f, -5.0f, 0.0f);
+    glEnd();
+}
 
 void idle() {
     if (!mouseDown)
@@ -44,6 +114,16 @@ void mouseMotion(int x, int y) {
 
 void myKeyboard(unsigned char key, int x, int y) {
     switch (key) {
+    case 'w':
+        xScale += 0.1;
+        yScale += 0.1;
+        zScale += 0.1;
+        break;
+    case 's':
+        xScale -= 0.1;
+        yScale -= 0.1;
+        zScale -= 0.1;
+        break;
     case 'x': // X axis
         xpoz = xpoz + 5;
         if (xpoz > 360) xpoz = 0;
@@ -120,14 +200,21 @@ void renderScene(void) {
 
     glLoadIdentity();
 
+
+    gluLookAt(0.0, 0.0, 100.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
     glTranslatef(a, b, c);
+    glScalef(xScale, yScale, zScale);
     glRotatef(xpoz, 1, 0, 0);
     glRotatef(ypoz, 0, 1, 0);
     glRotatef(zpoz, 0, 0, 1);
 
+    glScalef(xScale, yScale, zScale);
+
+
     glRotatef(xrot, 1.0f, 0.0f, 0.0f);
     glRotatef(yrot, 0.0f, 1.0f, 0.0f);
-    
+
     float d1[] = { 0.2, 0.5, 0.8, 1.0 };
     float d2[] = { 0.3, 0.5, 0.6, 1.0 };
     float d3[] = { 0.4, 0.2, 0.2, 1.0 };
@@ -135,46 +222,7 @@ void renderScene(void) {
     glMaterialfv(GL_FRONT, GL_DIFFUSE, d2);
     glMaterialfv(GL_FRONT, GL_DIFFUSE, d3);
 
-    // Front
-    glColor3f(1.0f, 0.0f, 0.0f);     // Red
-    glBegin(GL_TRIANGLES);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, 1.0f);
-    glEnd();
-
-    // Right
-    glColor3f(0.0f, 1.0f, 0.0f);     // Green
-    glBegin(GL_TRIANGLES);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(1.0f, -1.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glEnd();
-
-    // Back
-    glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-    glBegin(GL_TRIANGLES);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glEnd();
-
-    // Left
-    glColor3f(1.0f, 1.0f, 0.0f);       // Yellow
-    glBegin(GL_TRIANGLES);
-    glVertex3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-    glEnd();
-
-    // Bottom
-    glBegin(GL_QUADS);
-    glColor3f(1.0f, 1.0f, 1.0f);       // Black
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glVertex3f(1.0f, -1.0f, 1.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glEnd();
+    geladak();
 
     glutSwapBuffers();
 }
